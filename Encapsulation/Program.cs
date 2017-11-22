34e11dabcbe0cf9c6186a5066e6179a0c28f737a
Christopher Guerrero <christopherguerreroniu@gmail.com>
2017-11-22 16:32:16 -0600

    Importing project Encapsulation

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        /* this is just a sample program with using data encapsulation with the Animal class by creating Animal 
+         * object and using property methods of setters/getters.
+         */
+        static void Main(string[] args)
+        {
+   
+            Animal dog = new Animal();
+
+            //dog.Age = -12;
+            dog.Age = 2;
+            dog.Color = "Biege";
+            
+
+            Console.WriteLine("The dog's age is: " + dog.Age);
+            Console.WriteLine("The dog's fur color is: " + dog.Color);
+            dog.move();
+
+
+        }
+    }
+}
